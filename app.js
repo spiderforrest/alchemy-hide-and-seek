86cd1dae0bef7c1ee6f33a49d75b965aab13abf5
spiderforrest <spider@spood.org>
2022-10-31 14:36:51 -0700

    undoing last stretch goal because i don't like it

--- app.js
+++ app.js
@@ -82,10 +82,8 @@ function handleGuess(correctSpot, userGuess) {
     lossesEl.textContent = totalGuesses - correctGuesses;
 
     // and show the try again button/hide the others
-
-    // just uncommenting these lets more than one be right lol so that's a stretch goal
-    // shedButton.classList.add('hidden');
-    // treeButton.classList.add('hidden');
-    // boulderButton.classList.add('hidden');
+    shedButton.classList.add('hidden');
+    treeButton.classList.add('hidden');
+    boulderButton.classList.add('hidden');
     tryAgainButton.classList.remove('hidden');
 }
